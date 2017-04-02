86361e6f673e42e2286a01f58de23e956156c3b0
Paulo Roberto Cruz França <paulo.cruz9@gmail.com>
2017-04-01 21:20:26 -0300

    quase terminando uniao

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+function isFromLang(palavra, linguagem)
+{
+
+}
+
+function updateLangList()
+{
+    
+}
+
+function mergeLang(l1, l2)
+{
+    l2.forEach(function(t, index){
+        if(l1.indexOf(t) <= -1)
+        {
+            l1.push(t);
+        }
+    });
+
+    return l1;
+}
+
+function consoleLog(msg)
+{
+    divConsole.innerHTML += msg + "<br/>";
+}
+
+function transition(currentState, currentChar, strip, states)
+{            
+    if( typeof currentChar != "undefined")
+    {
+        if(alpha.indexOf(currentChar) <= -1 )
+        {        
+            return false;
+        }
+
+        if(",{} ".indexOf(currentChar) <= -1)
+        {
+            currentChar = "S";
+        }
+
+        if(strip.length >= 1)
+        {            
+            return transition(states[currentState][currentChar], strip[1], strip.slice(1), states);                            
+        }
+    }
+    else
+    {        
+        return states[currentState]["final"];
+    }    
+}
+
+//afd para reconhecimento da notação {s*(,s)*} ?
 var states =
 {
     0:{
@@ -54,41 +107,6 @@ alpha = "abcdefghijklmnopkrstuvxywz0123456789,}{ ";
 
 l = []
 
-
-function console_log(msg)
-{
-    divConsole.innerHTML += msg + "<br/>";
-}
-
-function transition(currentState, currentChar, strip, states)
-{        
-    // console.log("strip:",strip);
-    // console.log("currentChar:",currentChar);
-
-    if( typeof currentChar != "undefined")
-    {
-        if(alpha.indexOf(currentChar) <= -1 )
-        {        
-            return false;
-        }
-
-        if(",{} ".indexOf(currentChar) <= -1)
-        {
-            currentChar = "S";
-        }
-
-        if(strip.length >= 1)
-        {            
-            return transition(states[currentState][currentChar], strip[1], strip.slice(1), states);                            
-        }
-
-    }
-    else
-    {        
-        return states[currentState]["final"];
-    }    
-}
-
 btnInserir.addEventListener("click", function()
 {
     console.log("_________");    
@@ -118,21 +136,31 @@ btnInserir.addEventListener("click", function()
     else
     {
         console.log("nao reconhecido, erro de sintaxe");
-        console_log("nao reconhecido, erro de sintaxe");
+        consoleLog("nao reconhecido, erro de sintaxe");
     }
 });
 
+
+/*
+ junta dois alfabetos em um so e descarta os dois que foram unidos adicionando o total na lista
+ incompleto 
+*/
 btnUnir.addEventListener("click", function()
 {
     var inputs = document.getElementsByName("lg");
+    
+    var merged = [];
 
     inputs.forEach(function(t, index)
     {
         if(t.checked)
         {
             console.log("aaa");
+            merged = mergeLang(merged, l[t.value]);
         }
         console.log(l[t.value]);
     });
+    console.log(merged);
+    l.push(merged);
 });
 
